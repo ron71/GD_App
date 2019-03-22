ab0f4ddff642b70d9b9cab8701d233380ab70acc
Rohan Kumar <maxphinix@gmail.com>
2019-03-22 09:28:09 +0530

    Profile Edit Activity Added

---
+++ app/src/main/java/in/ac/kiit/justtalk/ProfileActivity.java
@@ -0,0 +1,14 @@
+package in.ac.kiit.justtalk;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+    }
+}
--- app/src/main/java/in/ac/kiit/justtalk/models/GDEvent.java
+++ app/src/main/java/in/ac/kiit/justtalk/models/GDEvent.java
@@ -1,10 +1,30 @@
 package in.ac.kiit.justtalk.models;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class GDEvent {
     String gdID;
     String organiserID;
-    HashMap<String, Scores> playerIDs = new HashMap<>();
+    String conductedBy;
+    String date;
+    String time;
+    String type;
+    String topic;
+    Map<String, Scores> playerIDs = new HashMap<>();
 
+    public  GDEvent(){
+
+    }
+    
+    public GDEvent(String gdID, String organiserID, String conductedBy, String date, String time, String type, String topic, Map<String, Scores> playerIDs) {
+        this.gdID = gdID;
+        this.organiserID = organiserID;
+        this.conductedBy = conductedBy;
+        this.date = date;
+        this.time = time;
+        this.type = type;
+        this.topic = topic;
+        this.playerIDs = playerIDs;
+    }
 }

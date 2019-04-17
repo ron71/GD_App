c00bca05ad3d511a4b93d8f815ab9b0eebc70dc2
Rohan Kumar <maxphinix@gmail.com>
2019-04-18 03:25:54 +0530

    The Last commit, V.19

---
+++ app/src/main/java/in/ac/kiit/justtalk/mailServices/Config.java
@@ -0,0 +1,14 @@
+
+/*
+*   https://www.simplifiedcoding.net/android-email-app-using-javamail-api-in-android-studio/
+* */
+
+package in.ac.kiit.justtalk.mailServices;
+
+
+
+public class Config {
+    public static final String EMAIL ="gdclubkiit@gmail.com";
+    public static final String PASSWORD ="letsGD@2019";
+}
+
